bfdcfea613394b409e68b046aaa7935d146fa8ef
todorokibase-code <todorokibase.25@gmail.com>
2025-12-27 15:54:39 +0900

    タイプスクリプトを導入

--- src/pages/Tictactoe.tsx
+++ src/pages/Tictactoe.tsx
@@ -5,62 +5,59 @@ import { StrictMode } from "react";
 import { useState } from 'react';
 import '../css/stylestictactoe.css';
 
-function Square({value,onSquareClick}:{value:string|null,onSquareClick:()=>void}) {
+function Square({ value, onSquareClick }: { value: string | null, onSquareClick: () => void }) {
   return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
 
-export default function Board() {
-  const [squares, setSquares] = useState(Array<string|null>(9).fill(null));
-  const [xIsNext, setXisNext] = useState<boolean>(true);
+export default function Board({ xIsNext, squares, onPlay }: { xIsNext: boolean, squares: Array<string | null>, onPlay: (a: Array<string | null>) => void }) {
   const winner = calculateWinner(squares);
   let status;
-  if(winner){
+  if (winner) {
     status = "Winner: " + winner;
-  }else {
+  } else {
     status = "Next: " + (xIsNext ? "x" : "o");
   }
 
 
 
-  function handleClick(i:number){
-    if(squares[i] || calculateWinner(squares)) return;
+  function handleClick(i: number) {
+    if (squares[i] || calculateWinner(squares)) return;
     const nextSquares = squares.slice();
-    if(xIsNext){
+    if (xIsNext) {
 
       nextSquares[i] = 'x'
-    }else{
+    } else {
       nextSquares[i] = 'o'
 
     }
-    setSquares(nextSquares);
-    setXisNext(!xIsNext);
+    onPlay(nextSquares);
 
   }
 
   return (
     <>
-    <div>winner:{status}</div>
+      <div>winner:{status}</div>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={()=> handleClick(0)}/>
-        <Square value={squares[1]} onSquareClick={()=> handleClick(1)}/>
-        <Square value={squares[2]} onSquareClick={()=> handleClick(2)}/>
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onSquareClick={()=> handleClick(3)}/>
-        <Square value={squares[4]} onSquareClick={()=> handleClick(4)}/>
-        <Square value={squares[5]} onSquareClick={()=> handleClick(5)}/>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onSquareClick={()=> handleClick(6)}/>
-        <Square value={squares[7]} onSquareClick={()=> handleClick(7)}/>
-        <Square value={squares[8]} onSquareClick={()=> handleClick(8)}/>
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   )
 }
 
-function calculateWinner(squares:Array<string | null>) {
+function calculateWinner(squares: Array<string | null>) {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -79,9 +76,31 @@ function calculateWinner(squares:Array<string | null>) {
   }
   return null;
 }
-const root:HTMLElement|null = document.getElementById("root");
+function Game() {
+  const [xIsNext, setXIsNext] = useState<boolean>(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+  function handlePlay(nextSquares: Array<string | null>) {
+    // TODO
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <div className='game'>
+
+      <div className='game-board'>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className='game-info'>
+        <ol></ol>
+      </div>
+    </div>
+  )
+}
+const root: HTMLElement | null = document.getElementById("root");
 ReactDOM.createRoot(root!).render(
   <StrictMode>
-    <Board />
+    <Game />
   </StrictMode>
 );

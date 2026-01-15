d4b97cea01de526c1b3cfeb502092e28bd8fbd77
todorokibase-code <todorokibase.25@gmail.com>
2026-01-15 20:36:32 +0900

    update: リンクの修正２回目

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
             <li><a href="./tictactoe.html">Tic-Tac-Toe</a></li>
             <li><a href="./router.html">ルーター</a></li>
             <li><a href="#">Git ブランチ間依存でコンフリクト発生テスト feature/Bで修正を追加しました</a></li>
-            <li><a href="#">Git ブランチ間依存でコンフリクト発生テスト 2回目</a></li>
+            <li><a href="#">Git feature/Dブランチで修正しました。</a></li>
           </ul>
 
         </nav>
